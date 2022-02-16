f48d103c3fa96b9b71804fdee58ea171ab4b1dfe
mirsini2003 <98818640+mirsini2003@users.noreply.github.com>
2022-02-16 20:52:50 +0200

    Add files via upload

---
+++ askisi_5.py.py
@@ -0,0 +1,45 @@
+with open('two_cities_ascii.txt') as f:
+    fin=f.read()
+    fin_mic=fin.lower()
+    lexeis=fin_mic.split()
+    
+    def syxnotita(lista,j):
+        #syxno=max(set(lista), key=lista.count)
+        print(j,max(set(lista), key=lista.count))
+        i=0
+        while i<len(lista):
+            if lista[i]==max(set(lista), key=lista.count):
+                lista.remove(max(set(lista), key=lista.count))
+            i+=1
+    
+    
+    print('Οι 10 δημοφιλέστερες λέξεις είναι: ')
+    for j in range (1,11):
+        print(j,max(set(lexeis), key=lexeis.count))
+        i=0
+        while i<len(lexeis):
+            if lexeis[i]==max(set(lexeis), key=lexeis.count):
+                lexeis.remove(max(set(lexeis), key=lexeis.count))
+            i+=1
+        
+    prwta_2_grammata=[x[:2] for x in lexeis]
+    print('Οι τρεις πρώτοι συνδυασμοί δύο πρώτων γραμμάτων που αρχίζουν οι περισσότερες λέξεις είναι: ')
+    for j in range (1,4):
+        print(j,max(set(prwta_2_grammata), key=prwta_2_grammata.count))
+        i=0
+        while i<len(prwta_2_grammata):
+            if prwta_2_grammata[i]==max(set(prwta_2_grammata), key=prwta_2_grammata.count):
+                prwta_2_grammata.remove(max(set(prwta_2_grammata), key=prwta_2_grammata.count))
+            i+=1
+    
+    prwta_3_grammata=[x[:3] for x in lexeis]
+    print('Οι τρεις πρώτοι συνδυασμοί τριών πρώτων γραμμάτων που αρχίζουν οι περισσότερες λέξεις είναι: ')
+    for j in range (1,4):
+        print(j,max(set(prwta_3_grammata), key=prwta_3_grammata.count))
+        i=0
+        while i<len(prwta_3_grammata):
+            if prwta_3_grammata[i]==max(set(prwta_3_grammata), key=prwta_3_grammata.count):
+                prwta_3_grammata.remove(max(set(prwta_3_grammata), key=prwta_3_grammata.count))
+            i+=1
+
+
